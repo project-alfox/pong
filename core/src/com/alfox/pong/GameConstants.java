f388cdf6a7e371ae6bb19baa0b33aa62b245a7e0
Coltin Caverhill <ultranova@gmail.com>
2019-02-24 14:59:51 -0500

    Add game constants file

---
+++ core/src/com/alfox/pong/GameConstants.java
@@ -0,0 +1,8 @@
+package com.alfox.pong;
+
+public class GameConstants {
+  public static final String TITLE = "Alfox Pong";
+  public static final int SCREEN_WIDTH = 1280;
+  public static final int SCREEN_HEIGHT = 720;
+  public static final boolean Y_DOWN = false;
+}
--- desktop/src/com/alfox/pong/desktop/DesktopLauncher.java
+++ desktop/src/com/alfox/pong/desktop/DesktopLauncher.java
@@ -1,16 +1,25 @@
 package com.alfox.pong.desktop;
 
+import com.alfox.pong.GameConstants;
 import com.alfox.pong.PongGdxGame;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.Color;
 
 public class DesktopLauncher {
   public static void main(String[] arg) {
     LwjglApplicationConfiguration applicationConfig = new LwjglApplicationConfiguration();
+    applicationConfig.title = GameConstants.TITLE;
+    applicationConfig.width = GameConstants.SCREEN_WIDTH;
+    applicationConfig.height = GameConstants.SCREEN_HEIGHT;
+    applicationConfig.initialBackgroundColor = Color.BLACK;
     applicationConfig.resizable = false;
     applicationConfig.backgroundFPS = 10;
     applicationConfig.foregroundFPS = 60;
-    applicationConfig.title = "Alfox Pong";
+    applicationConfig.allowSoftwareMode = false;
+    applicationConfig.fullscreen = false;
+    // can be changed at runtime via {@link Graphics#setVSync(boolean)}
+    applicationConfig.vSyncEnabled = true;
     new LwjglApplication(new PongGdxGame(), applicationConfig);
   }
 }
